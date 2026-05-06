d9eab6f2227406e62e35e0ee2cbeb175b15ea257
jvictornascimento <victojoao789@gmail.com>
2026-05-06 00:17:35 -0300

    chore(config): add dev test and prod profiles

--- src/test/java/com/jvictornascimento/accessmanager/AccessManagerApplicationTests.java
+++ src/test/java/com/jvictornascimento/accessmanager/AccessManagerApplicationTests.java
@@ -1,13 +1,33 @@
 package com.jvictornascimento.accessmanager;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@AutoConfigureTestRestTemplate
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AccessManagerApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void healthEndpointShouldBeAvailable() {
+		var response = restTemplate.getForEntity("/actuator/health", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).contains("\"status\":\"UP\"");
 	}
 
 }
